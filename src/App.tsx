e76789fa6d90c11738e3248b7fa8c642594a3d67
Ronan Morais <ronan86@gmail.com>
2023-05-11 01:01:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+import Nav from './components/Nav';
+
+const App: Component = () => {
+  return (
+    <div>
+      <Nav/>
+      <h1>App Component </h1>
+    </div>
+  );
+};
+
+export default App;
