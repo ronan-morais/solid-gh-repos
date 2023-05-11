ed51a49c51af51c7830089af8fe3860fa0269728
Ronan Morais <ronan86@gmail.com>
2023-05-11 18:53:10 -0300

    save

--- src/pages/SavedRepos.tsx
+++ src/pages/SavedRepos.tsx
@@ -1,9 +1,13 @@
-import { Component } from "solid-js";
+import { Component, For, createSignal } from "solid-js";
+import RepoCard, { Repo } from "../components/RepoCard";
+
+const [savedRepos, setSavedRepos] = createSignal([]);
 
 const SavedRepos: Component = () => {
   return (
     <div>
-      <h2>SavedRepos</h2>
+      <h2>Your saved repos</h2>
+      <For each={savedRepos()}>{(repo: Repo) => <RepoCard repo={repo} />}</For>
     </div>
   );
 };
